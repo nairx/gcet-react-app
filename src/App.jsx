98337958754f647b1aa64498d48984937d7bf94b
nairxprv@gmail.com <praveannair@gmail.com>
2025-05-21 20:51:12 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,33 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div>
-      <header>
-        <h1>My Online Shop</h1>
-        <hr />
-      </header>
-      <main>
-        <h2>Product List</h2>
-      </main>
-      <footer>
-        <hr />
-        &copy; 2005. All rights Reserved.
-      </footer>
+      <BrowserRouter>
+        <header>
+          <h1>My Online Shop</h1>
+          <Link to="/">Home</Link>-
+          <Link to="/cart">Cart</Link>
+          <hr />
+        </header>
+
+        <main>
+          <Routes>
+            <Route index element={<Product />} />
+            <Route path="/" element={<Product/>}></Route>
+            <Route path="/cart" element={<Cart/>}></Route>
+          </Routes>
+        </main>
+
+        <footer>
+          <hr />
+          &copy; 2005. All rights Reserved.
+        </footer>
+      </BrowserRouter>
     </div>
   );
 }
